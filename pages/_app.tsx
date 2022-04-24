3bd24ecccf179fea21e9276f0a7695599bcf451c
=Viktor <maricevviktor2@gmail.com>
2022-04-24 13:38:57 +0600

    feat:progressbar added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,21 @@
 import { useEffect, useState } from "react";
 import "../styles/globals.css";
+import NextNProgress from "nextjs-progressbar";
 import type { AppProps } from "next/app";
 import { wrapper } from "../src/redux/store";
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <NextNProgress
+        color="#fdfdfd"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={3}
+        showOnShallow={true}
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default wrapper.withRedux(MyApp);
