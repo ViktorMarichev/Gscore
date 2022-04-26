5568a759aeab4f23097c9118d9a85296b45e93a5
=Viktor <maricevviktor2@gmail.com>
2022-04-26 20:09:41 +0600

    feat:Curtain added

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,7 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styled from "styled-components";
+import Container from "src/components/Container";
 import HeaderContainer from "src/components/HeaderContainer";
 import Footer from "src/components/Footer";
+import useResizeObserver from "use-resize-observer";
+import Curtain from "src/components/Curtain";
 import { useRouter } from "next/router";
 import { UserSelectors } from "src/redux/User";
 import { useAppSelector } from "src/redux/store";
@@ -10,6 +13,8 @@ type MainLayoutProps = {
 };
 
 const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
+  const [isSmall, setIsSmal] = useState(false);
+  const [curtain, setCurtain] = useState(false);
   const user = useAppSelector((state) => UserSelectors.userData(state));
   const availablePaths = ["/", "/subscribing/[id]"];
 
@@ -20,12 +25,53 @@ const MainLayout: React.FC<MainLayoutProps> = ({ children }) => {
     }
   }, [user]);
 
+  const { ref, width } = useResizeObserver({
+    onResize: ({ width, height }) => {
+      if (width! <= 640) {
+        setIsSmal(true);
+      } else {
+        setIsSmal(false);
+      }
+    },
+  });
+  const toggleCurtainHandler = () => {
+    setCurtain((prev) => !prev);
+  };
+
   return (
-    <>
-      <HeaderContainer />
-      <main>{children}</main>
-      <Footer />
-    </>
+    <MainLayoutWrapper>
+      <Container>
+        <HeaderContainer
+          isSmall={isSmall}
+          toggleCurtain={toggleCurtainHandler}
+        />
+        <main ref={ref}>{children}</main>
+        <Footer />
+      </Container>
+      {curtain && isSmall ? (
+        <CurtainWrapper
+          onClick={() => {
+            setCurtain(false);
+          }}
+        >
+          <Curtain toggleCurtain={toggleCurtainHandler} />
+        </CurtainWrapper>
+      ) : null}
+    </MainLayoutWrapper>
   );
 };
+const MainLayoutWrapper = styled.div`
+  position: relative;
+`;
+
+const CurtainWrapper = styled.div`
+  position: fixed;
+  display: flex;
+  justify-content: flex-end;
+  height: 100%;
+  width: 100%;
+  top: 0px;
+  background: rgba(0, 0, 0, 0.589);
+`;
+
 export default MainLayout;
